777c8fc9989c07f6f938477cd0a654f2e64533cd
Kyrylo Savieliev <kirillofficial0507@gmail.com>
2025-02-09 22:19:20 +0200

    fix: wrap template into ClientOnly component to prevent hydration mismatch

--- apps/client/components/valentines/TemplateOne.vue
+++ apps/client/components/valentines/TemplateOne.vue
@@ -7,14 +7,15 @@ import Cloud5 from '~/assets/template-one/cloud-5.svg';
 import SVGMan from '~/assets/template-one/man.svg';
 import Star from '~/assets/template-one/star.svg';
 
-const props = defineProps<{ text: string; from: string; to: string }>();
+defineProps<{ text: string; from: string; to: string }>();
+
 const cardRef = ref(null);
 const isVisible = ref(false);
 
 const random = (min: number, max: number) => Math.random() * (max - min) + min;
 
-const stars = ref(
-  Array.from({ length: 15 }, () => {
+const stars = computed(() => {
+  return Array.from({ length: 15 }, () => {
     const size = random(20, 50);
     return {
       top: `${random(4, 96 - (size * 100) / 720)}%`,
@@ -23,18 +24,16 @@ const stars = ref(
       rotation: `rotate(${random(0, 360)}deg)`,
       delay: random(0, 1),
     };
-  }),
-);
-
-const cloudDelays = computed(() => {
-  return {
-    cloud1: random(0, 2),
-    cloud2: random(0, 2),
-    cloud3: random(0, 2),
-    cloud4: random(0, 2),
-    cloud5: random(0, 2),
-  };
+  });
 });
+
+const cloudDelays = computed(() => ({
+  cloud1: random(0, 2),
+  cloud2: random(0, 2),
+  cloud3: random(0, 2),
+  cloud4: random(0, 2),
+  cloud5: random(0, 2),
+}));
 
 useIntersectionObserver(cardRef, ([{ isIntersecting }]) => {
   if (isIntersecting && !isVisible.value) {
@@ -44,79 +43,81 @@ useIntersectionObserver(cardRef, ([{ isIntersecting }]) => {
 </script>
 
 <template>
-  <main
-    class="flex flex-col w-full h-dvh bg-[#fface1] items-center justify-center md:p-4"
-  >
-    <div
-      ref="cardRef"
-      :class="[
-        'md:h-full md:w-full lg:h-[720px] lg:w-[1280px] h-full w-full bg-white border-2 border-black p-4 relative hover:shadow-xl transition',
-        isVisible ? 'animate-card-appear' : 'opacity-0',
-      ]"
+  <ClientOnly>
+    <main
+      class="flex flex-col w-full h-dvh bg-[#fface1] items-center justify-center md:p-4"
     >
-      <Cloud2
-        :class="'hidden lg:block absolute bottom-[-2px] left-[-2px] h-[15%] z-10 animate-cloud-float'"
-        :style="{ animationDelay: `${cloudDelays.cloud2}s` }"
-      />
-      <Cloud3
-        :class="'hidden lg:block absolute bottom-[-2px] right-[30%] h-[20%] z-20 animate-cloud-float'"
-        :style="{ animationDelay: `${cloudDelays.cloud3}s` }"
-      />
-      <Cloud4
-        :class="'hidden lg:block absolute bottom-[5%] right-[-2px] h-[20%] z-20 animate-cloud-float'"
-        :style="{ animationDelay: `${cloudDelays.cloud4}s` }"
-      />
       <div
-        class="overflow-hidden size-full border-black border-2 bg-[#fecdeb] relative bg-[length:30px_30px] bg-[linear-gradient(to_right,#fee0f1_1px,transparent_1px),linear-gradient(to_bottom,#fee0f1_1px,transparent_1px)]"
+        ref="cardRef"
+        :class="[
+          'md:h-full md:w-full lg:h-[720px] lg:w-[1280px] h-full w-full bg-white border-2 border-black p-4 relative hover:shadow-xl transition',
+          isVisible ? 'animate-card-appear' : 'opacity-0',
+        ]"
       >
-        <Star
-          v-for="(star, index) in stars"
-          :key="index"
-          class="absolute z-30 animate-star-twinkle"
-          :style="{
-            top: star.top,
-            left: star.left,
-            width: star.size,
-            height: star.size,
-            transform: star.rotation,
-            animationDelay: `${star.delay}s`,
-          }"
+        <Cloud2
+          :class="'hidden lg:block absolute bottom-[-2px] left-[-2px] h-[15%] z-10 animate-cloud-float'"
+          :style="{ animationDelay: `${cloudDelays.cloud2}s` }"
         />
-
-        <p
-          v-html="text.replaceAll('\n', '<br>')"
-          class="absolute left-[2%] md:top-[2%] text-wrap text-[#e9110b] font-excali lg:w-[60%] z-[45] md:text-7xl text-4xl pointer-events-none"
+        <Cloud3
+          :class="'hidden lg:block absolute bottom-[-2px] right-[30%] h-[20%] z-20 animate-cloud-float'"
+          :style="{ animationDelay: `${cloudDelays.cloud3}s` }"
         />
-
-        <p
-          class="absolute font-excali left-[6%] bottom-[2%] text-wrap text-black font-semibold z-[45] text-3xl pointer-events-none"
+        <Cloud4
+          :class="'hidden lg:block absolute bottom-[5%] right-[-2px] h-[20%] z-20 animate-cloud-float'"
+          :style="{ animationDelay: `${cloudDelays.cloud4}s` }"
+        />
+        <div
+          class="overflow-hidden size-full border-black border-2 bg-[#fecdeb] relative bg-[length:30px_30px] bg-[linear-gradient(to_right,#fee0f1_1px,transparent_1px),linear-gradient(to_bottom,#fee0f1_1px,transparent_1px)]"
         >
-          Від щирого серця<br />
-          для {{ to }}<br />
-          від {{ from }}
-        </p>
+          <Star
+            v-for="(star, index) in stars"
+            :key="index"
+            class="absolute z-30 animate-star-twinkle"
+            :style="{
+              top: star.top,
+              left: star.left,
+              width: star.size,
+              height: star.size,
+              transform: star.rotation,
+              animationDelay: `${star.delay}s`,
+            }"
+          />
 
-        <p
-          class="absolute w-full text-center bottom-[-10%] left-1/2 -translate-x-1/2 font-excali pointer-events-none lg:block hidden"
-        >
-          З любовʼю від IT-відділу Студентського сенату ХНУРЕ!
-        </p>
+          <p
+            v-html="text.replaceAll('\n', '<br>')"
+            class="absolute left-[2%] md:top-[2%] text-wrap text-[#e9110b] font-excali lg:w-[60%] z-[45] md:text-7xl text-4xl pointer-events-none"
+          />
 
-        <SVGMan
-          class="absolute lg:bottom-[20%] lg:right-[2%] lg:h-3/4 md:h-[30%] h-1/3 max-lg:bottom-[35%] max-lg:left-1/2 max-lg:-translate-x-1/2 z-20"
-        />
+          <p
+            class="absolute font-excali left-[6%] bottom-[2%] text-wrap text-black font-semibold z-[45] text-3xl pointer-events-none"
+          >
+            Від щирого серця<br />
+            для {{ to }}<br />
+            від {{ from }}
+          </p>
 
-        <Cloud1
-          :class="'absolute lg:bottom-[23%] lg:right-[43%] lg:h-[25%] h-[15%] right-0 z-10 bottom-[-2px] animate-cloud-float'"
-          :style="{ animationDelay: `${cloudDelays.cloud1}s` }"
-        />
-        <Cloud5
-          :class="'absolute lg:top-[1%] md:right-[5%] max-md:left-[3%] lg:h-[35%] h-[16%] top-[-2px] z-10 animate-cloud-float'"
-          :style="{ animationDelay: `${cloudDelays.cloud5}s` }"
-        />
+          <p
+            class="absolute w-full text-center bottom-[-10%] left-1/2 -translate-x-1/2 font-excali pointer-events-none lg:block hidden"
+          >
+            З любовʼю від IT-відділу Студентського сенату ХНУРЕ!
+          </p>
+
+          <SVGMan
+            class="absolute lg:bottom-[20%] lg:right-[2%] lg:h-3/4 md:h-[30%] h-1/3 max-lg:bottom-[35%] max-lg:left-1/2 max-lg:-translate-x-1/2 z-20"
+          />
+
+          <Cloud1
+            :class="'absolute lg:bottom-[23%] lg:right-[43%] lg:h-[25%] h-[15%] right-0 z-10 bottom-[-2px] animate-cloud-float'"
+            :style="{ animationDelay: `${cloudDelays.cloud1}s` }"
+          />
+          <Cloud5
+            :class="'absolute lg:top-[1%] md:right-[5%] max-md:left-[3%] lg:h-[35%] h-[16%] top-[-2px] z-10 animate-cloud-float'"
+            :style="{ animationDelay: `${cloudDelays.cloud5}s` }"
+          />
+        </div>
       </div>
-    </div>
-  </main>
+    </main>
+  </ClientOnly>
 </template>
 
 <style>
