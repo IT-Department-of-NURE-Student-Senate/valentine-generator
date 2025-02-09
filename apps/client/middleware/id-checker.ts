320f16ffc37e32829a87f936a1dd3e632799ff49
Kyrylo Savieliev <kirillofficial0507@gmail.com>
2025-02-09 21:09:32 +0200

    chore: add id checker to page for creating valentine cards

--- apps/client/middleware/id-checker.ts
+++
@@ -1,7 +0,0 @@
-export default defineNuxtRouteMiddleware((from, to) => {
-  const valentineId = useCookie('valentine-card-id');
-
-  if (!valentineId || to.params.id !== valentineId.value) {
-    return navigateTo('/');
-  }
-});
--- apps/client/pages/[id]/edit.vue
+++ apps/client/pages/[id]/edit.vue
@@ -1,9 +1,23 @@
 <script lang="ts" setup>
-const valentineId = ref(String(useRouteParams('id')));
+import { VALENTINE_COOKIE_KEY } from '~/constants';
+
+const cookie = useCookie(VALENTINE_COOKIE_KEY, {
+  watch: true,
+});
+
+const id = useRouteParams('id');
+
+if (!cookie.value || cookie.value !== id.value) {
+  throw showError({
+    status: 400,
+    message: 'Упс... ви не можете відкрити цю сторінку',
+    fatal: true,
+  });
+}
 </script>
 
 <template>
   <div class="flex flex-col gap-4 size-full items-center justify-center">
-    <Stepper :id="valentineId" />
+    <Stepper :id="id as string" />
   </div>
 </template>
