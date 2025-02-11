dc362df1158119fd80bdfe8c1a3e69c239601e9b
perkinson1251 <tttrashutin@gmail.com>
2025-02-11 11:11:19 +0200

    fix(client): return the gradient to the default template

--- apps/client/layouts/default.vue
+++ apps/client/layouts/default.vue
@@ -1,7 +1,13 @@
 <script lang="ts" setup></script>
 
 <template>
-  <main class="flex flex-col w-full min-h-dvh items-center justify-center p-0">
+  <main
+    class="flex flex-col w-full min-h-dvh items-center justify-center p-0"
+    :style="{
+      background:
+        'radial-gradient(94.39% 76.37% at 50% 134.15%, #E11C47 0%, #FFF 100%)',
+    }"
+  >
     <slot />
   </main>
 </template>
